fd12fb5dc15212cd4a21894d829dce6e7220fdc4
Mohamed Rashad <engineer.rachad@gmail.com>
2024-03-06 17:04:19 +0200

    Files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Library.Controllers
+{
+    public class HomeController : Controller
+    {
+        //constractour
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //localhost:7277/home/privacy
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
+
+
+
+
+
