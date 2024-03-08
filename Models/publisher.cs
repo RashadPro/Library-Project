bd4e35a514c88599b7d1de3d73992353c7735ad5
Ahmed <ahmeed02003@gmail.com>
2024-03-08 22:50:27 +0200

    publisher added

---
+++ Models/publisher.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Models
+{
+    public class publisher
+    {
+        public int pup_ISBN { get; set; }
+        public string name { get; set; }
+        public string location { get; set; }
+        public int phone { get; set; }
+    }
+}
--- obj/Debug/net6.0/Library.AssemblyInfo.cs
+++ obj/Debug/net6.0/Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca31c07c558bc8de2eca00e2cd5ad30a5651d57")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
