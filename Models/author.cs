981f087d27a27f25b4a09d9188bc33f15571d184
Mohamed Rashad <engineer.rachad@gmail.com>
2024-03-09 14:18:24 +0200

    Class author and author_book

---
+++ Models/author.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class author
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string address { get; set; }
+        public string Email { get; set; }
+        public int phone { get; set; }
+        public string country { get; set; }
+
+        //List Of author_books
+        public List<author_book> author_books { get; set; }
+    }
+}
