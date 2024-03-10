160e98fd9cd9ac9718fb2c7b9ac25d4eb3931ffe
Ahmed <ahmeed02003@gmail.com>
2024-03-11 00:53:25 +0200

    kkkkkk

--- obj/Debug/net6.0/Library.AssemblyInfo.cs
+++ obj/Debug/net6.0/Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+250fdcf8a7fe0dfa38007ae856832f7fd46b984c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
