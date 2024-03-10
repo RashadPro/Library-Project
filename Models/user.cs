ac8cdb8e6b70190e9167d8574279affccd352a2e
WalaaElahtam <walaadeyaa20@gmail.com>
2024-03-10 22:11:47 +0200

    first commit

---
+++ Models/user.cs
@@ -0,0 +1,19 @@
+﻿namespace Library.Models
+{
+    public class user
+    {
+        public int ISBN { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public int age { get; set; }
+        public string email { get; set; }
+        public string address { get; set; }
+        public int phone { get; set; }
+        //pk payment
+        public List <payment> payments { get; set; }
+        //invoice
+        public List <invoice> invoices { get; set; }
+        //prder
+        public List <order> orders { get; set; }
+    }
+}
