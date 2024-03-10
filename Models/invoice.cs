69aa0f4ba5f581dfe7484401848b326971823c91
nawal bahaa <n.elahtam3525@su.edu.eg>
2024-03-10 22:27:57 +0200

    first commit

---
+++ Models/invoice.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.Models
+{
+    public class invoice
+    {
+        public int invoice_ISBN { get; set; }
+        public int cust_num { get; set; }
+        public int date { get; set; }
+        public int amount { get; set; }
+        //pk payment
+        public List<payment> Payments { get; set; }
+
+    }
+}
---
+++ Models/payment.cs
@@ -0,0 +1,17 @@
+﻿namespace Library.Models
+{
+    public class payment
+    {
+        public int payment_ISBN { get; set; }
+        public DateTime data { get; set; }
+        public int amount { get; set; }
+        public string states { get; set; }
+
+
+        //pk invoice
+        public List<invoice> invoices { get; set; }
+
+        //pk user
+        public List<user> user { get; set; }
+    }
+}
