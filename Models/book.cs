c92b6a79655834ce90137e76252fa3a5615c31b1
Mohamed Rashad <engineer.rachad@gmail.com>
2024-03-11 00:35:05 +0200

    Class Book and Book Type

---
+++ Models/book.cs
@@ -0,0 +1,16 @@
+﻿namespace Library.Models
+{
+    public class book
+    {
+        public int book_ISBN { get; set; }
+        public string version { get; set; }
+        public string name { get; set; }
+        public DateTime date_of_publication { get; set; }
+        public int quantity { get; set; }
+        public publisher Publisher { get; set; }
+        public int pup_ISBN { get; set; }
+        public booktype BookType { get; set; }
+        public List<author_book> Author_Book { get; set; }
+
+    }
+}
---
+++ Models/booktype.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Models
+{
+    public class booktype
+    {
+        public int book_ISBN { get; set; }
+        public string type { get; set; }
+        public book Book { get; set; }
+
+    }
+}
